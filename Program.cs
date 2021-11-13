3eda68e40afb22feb5e7f754071a1930bcc713ff
Jean Barbosa <jeanbarbosa.it@gmail.com>
2021-11-13 14:01:43 -0300

    SolutionAdicionar .gitignore e .gitattributes.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AppPosicaoQuadrantePlano
+{
+    /// <summary>
+    /// Exercicios livres.
+    /// Programa para calcular a posição em um quadrante dado os valores de X e Y.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double X, Y;
+            string posicao;
+
+            Console.WriteLine("Informe o valor de X e Y para receber a posição do quadrante: ");
+            string[] vet = Console.ReadLine().Split(" ");
+            
+            X = double.Parse(vet[0]);
+            Y = double.Parse(vet[1]);
+            
+            Quadrante quadrante = new Quadrante(X,Y);
+            posicao = quadrante.VerificaQuatrante();
+
+            Console.WriteLine("A posição está no {0}", posicao);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Quadrante.cs
@@ -0,0 +1,41 @@
+﻿namespace AppPosicaoQuadrantePlano
+{
+    class Quadrante
+    {
+        private double x;
+        private double y;
+
+        public Quadrante(double X, double Y) 
+        {
+            this.x = X;
+            this.y = Y;
+        }
+
+        public string VerificaQuatrante() 
+        {
+            string msg = "";
+
+            if (x == 0 && y == 0)
+            {
+                msg="Ponto de origem!";
+            }
+            else if (x >= 0 && y >= 0)
+            {
+                msg= "1º Quadrante!";
+            }
+            else if (x<=0 && y>=0)
+            {
+                msg= "2º Quadrante!";
+            }
+            else if (x <= 0 && y <= 0)
+            {
+                msg= "3º Quadrante!";
+            }
+            else if (x >= 0 && y <= 0)
+            {
+                msg= "4º Quadrante!";
+            }
+            return msg;
+        }
+    }
+}
